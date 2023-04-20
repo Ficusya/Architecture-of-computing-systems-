8e11a3e69f2e409dff9a7af60abda27f4c8b4252
Ficusya <128972041+Ficusya@users.noreply.github.com>
2023-04-20 10:39:23 +0300

    Add files via upload

---
+++ 5 prot.asm
@@ -0,0 +1,73 @@
+; count amount of negatives in c <= A[i] <= d
+org 100h
+.Data
+N DD 5
+A DD 9999h,-35000h,35000h,-20900h,1234h
+c DD -35000h
+d DD 36000h
+.Code
+mov cx,N
+mov dx,0
+mov si, offset A
+loop_start:
+    mov ax, word ptr [SI]+2
+    cmp ax,0h
+    jge skip
+    
+    mov bx, word ptr d+2
+    cmp ax,bx
+    jg skip
+    je equal_d
+    jl _c 
+
+equal_d:
+    mov ax,word ptr [SI]
+    mov bx, word ptr d
+    cmp ax,bx
+    jg skip
+_c:
+    mov ax, word ptr [SI]+2
+    mov bx, word ptr c+2
+    cmp ax,bx
+    jl skip
+    je equal_c
+    jg skip_good
+equal_c:
+    mov ax, word ptr [SI]
+    mov bx, word ptr c
+    cmp ax,bx
+    jl skip         
+skip_good:
+    inc dx    
+skip:
+    add SI,4
+    loop loop_start
+
+MOV AX,DX
+print       proc
+        push    ax
+        push    bx
+        push    cx
+        push    dx
+        mov     bx,     10          
+        mov     cx,     0            
+        @@div:
+                xor     dx,     dx     
+                div     bx
+                add     dl,     '0'   
+                push    dx             
+                inc     cx              
+                test    ax,     ax     
+        jnz     @@div                 
+        @@show:
+                mov     ah,     02h 
+                pop     dx             
+                int     21h            
+        loop    @@show                 
+    pop     dx
+    pop     cx
+    pop     bx
+    pop     ax
+        ret
+print       endp
+ret  
